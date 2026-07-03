365abe576b6642884b41525c2e274a216f8065c3
azmi1500 <abdulazmi1500@gmail.com>
2026-07-04 01:52:38 +0800

    dayend adds on final i think

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform, Alert } from 'react-native';  // ✅ Add Alert
+import { Platform, Alert } from 'react-native';  // ✅ Add Alert yjn
 
 // We'll use this to navigate
 let navigateToLogin = null;
